dbaebebbb1adcb9b47990270aa033c1c4ef68a9a
Dan Weinberg <dan.weinberg@armakuni.com>
2019-08-12 11:06:22 +0100

    Install bundler

--- scripts/acceptance-tests.sh
+++ scripts/acceptance-tests.sh
@@ -3,7 +3,8 @@
 SCRIPT_DIR=$(dirname $0)
 ROOT=$(cd $SCRIPT_DIR/..; pwd)
 
-pushd $ROOT > /dev/null
-  bundle install
-  bundle exec rspec -t 'acceptance'
-popd > /dev/null
+cd $ROOT
+
+gem install bundler
+bundle install
+bundle exec rspec -t 'acceptance'
