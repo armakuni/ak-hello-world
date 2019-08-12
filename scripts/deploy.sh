4ba4086acd836feaceec014440be33837caee7d5
Dan Weinberg <dan.weinberg@armakuni.com>
2019-08-12 12:29:37 +0100

    Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,5 +7,8 @@ SERVICE_ACCOUNT_JSON='svc_account.json'
 cd $ROOT
 echo $SERVICE_ACCOUNT_KEY_BASE64 | base64 --decode > $SERVICE_ACCOUNT_JSON
 
-gcloud auth activate-service-account --key-file  --project $PROJECT_ID
-gloud app deploy
+gcloud auth activate-service-account \
+  --key-file=$SERVICE_ACCOUNT_JSON \
+  --project=$PROJECT_ID
+
+gcloud app deploy
