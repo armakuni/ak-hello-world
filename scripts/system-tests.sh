8e119dccaecb682b5651498350c85cca6bcc742d
Dan Weinberg <dan.weinberg@armakuni.com>
2019-08-14 14:40:41 +0100

    Uncomment script lines

--- scripts/system-tests.sh
+++ scripts/system-tests.sh
@@ -7,15 +7,15 @@ cd $ROOT
 
 export APP_ENDPOINT="https://ak-demo-249610.appspot.com/"
 
-# apt-get update && \
-#   apt-get install -y \
-#   ruby-chromedriver-helper \
-#   xvfb \
-#   libxi6 \
-#   libgconf-2-4 \
-#   chromium \
-#   chromium-driver
-#
-# gem install bundler
-# bundle install
+apt-get update && \
+  apt-get install -y \
+  ruby-chromedriver-helper \
+  xvfb \
+  libxi6 \
+  libgconf-2-4 \
+  chromium \
+  chromium-driver
+
+gem install bundler
+bundle install
 bundle exec cucumber
